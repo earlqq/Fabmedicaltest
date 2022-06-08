aaf363c64efa4cfdade8d5d83af34e5172680ff6
Your Name <your@email.com>
2022-06-08 10:43:54 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("35c37abd-bbae-49ae-afd3-82cf44b1bd74");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
